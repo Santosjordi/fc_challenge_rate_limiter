a129512a1bc96dada9e2d1adff8746e8379d2329
jordiws <jordiws@gmail.com>
2025-04-21 18:40:12 +0000

    added viper for app configuration

--- config/config.go
+++ config/config.go
@@ -1,4 +1,28 @@
 package config
 
+// This project uses viper to load the variables from
+// the .env file in the root of the project
 type conf struct {
+	Address  string
+	Password string
+	DB       int   
+}
+
+func LoadConfig(path string) (*conf, error) {
+	var cfg *conf
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, nil
 }
--- internal/infra/db/rateLimiterRepository.go
+++ internal/infra/db/rateLimiterRepository.go
@@ -3,3 +3,12 @@ package db
 type Cache struct {
 	client *redis.Client
 }
+
+func NewCache() *Cache {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	return &Cache{client: client}
+)
